c93cfbcb1ee6b488c1b1be423b7e9be450b57ad1
Mike de Boer <info@mikedeboer.nl>
2022-01-14 15:30:27 +0100

    Disable rules 'lines-between-class-members', 'no-await-in-loop' and allow the `for...of` iterator pattern.

--- index.js
+++ index.js
@@ -34,7 +34,27 @@ module.exports = {
 				SwitchCase: 1,
 			},
 		],
+		'lines-between-class-members': 'off',
+		'no-await-in-loop': 'off',
 		'no-console': 'error',
 		'no-tabs': 'off',
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'ForInStatement',
+				message:
+					'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+			},
+			{
+				selector: 'LabeledStatement',
+				message:
+					'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+			},
+			{
+				selector: 'WithStatement',
+				message:
+					'`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+			},
+		],
 	}),
 };
